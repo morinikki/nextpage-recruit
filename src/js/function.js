754d70c5988a6fc91bede1a213239c7ed90f6a10
Yonamine Airi <petit.chouette1422@gmail.com>
2019-08-18 02:07:24 +0900

    headerふわってなるやつ！

--- src/js/function.js
+++ src/js/function.js
@@ -12,6 +12,7 @@ $(function () {
 	});
 });
 
+// TOPページのエントリー・お問い合わせbtn
 $(window).scroll(function() {
 	if($(this).scrollTop() > 500) {
 		$("#entry-btn").css("opacity","1");
@@ -22,5 +23,31 @@ $(window).scroll(function() {
 		$("#contact-btn").css("opacity","1");
 	} else {
 		$("#contact-btn").css("opacity","0");
+	}
+});
+
+$(window).scroll(function() {
+	if($(this).scrollTop() > 500) {
+		$(".header").css("background","rgba(255,255,255, .8)");
+		$(".header").css("box-shadow","0px 2px 5px 2px rgba(140, 140, 140, 0.4)");
+	} else {
+		$(".header").css("background","none");
+		$(".header").css("box-shadow","none");
+	}
+	if($(this).scrollTop() > 500) {
+		$("#menu li a").css("margin-left","50px");
+		$("#menu li a").css("font-size","1.2rem");
+		$("#menu li a").css("font-weight","bold");
+		$("#menu li a").css("color","#000");
+	} else {
+		$("#menu li a").css("margin-left","40px");
+		$("#menu li a").css("font-size","1.4rem");
+		$("#menu li a").css("font-weight","normal");
+		$("#menu li a").css("color","#fff");
+	}
+	if($(this).scrollTop() > 500) {
+		$(".header-logo a").css("width","150px");
+	} else {
+		$(".header-logo a").css("width","175px");
 	}
 });
