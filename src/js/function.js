c3c707dcda08f774d7aa146599d1cfe027762665
Yonamine Airi <petit.chouette1422@gmail.com>
2019-08-18 16:32:27 +0900

    sp事業内容でけた、

--- src/js/function.js
+++ src/js/function.js
@@ -26,28 +26,28 @@ $(window).scroll(function() {
 	}
 });
 
-$(window).scroll(function() {
-	if($(this).scrollTop() > 500) {
-		$(".header").css("background","rgba(255,255,255, .5)");
-		$(".header").css("box-shadow","0px 2px 5px 2px rgba(140, 140, 140, 0.4)");
-	} else {
-		$(".header").css("background","none");
-		$(".header").css("box-shadow","none");
-	}
-	if($(this).scrollTop() > 500) {
-		$("#menu li a").css("margin-left","50px");
-		$("#menu li a").css("font-size","1.3rem");
-		$("#menu li a").css("font-weight","bold");
-		$("#menu li a").css("color","#000");
-	} else {
-		$("#menu li a").css("margin-left","40px");
-		$("#menu li a").css("font-size","1.4rem");
-		$("#menu li a").css("font-weight","normal");
-		$("#menu li a").css("color","#fff");
-	}
-	if($(this).scrollTop() > 500) {
-		$(".header-logo a").css("width","150px");
-	} else {
-		$(".header-logo a").css("width","175px");
-	}
-});
+// $(window).scroll(function() {
+// 	if($(this).scrollTop() > 500) {
+// 		$(".header").css("background","rgba(255,255,255, .5)");
+// 		$(".header").css("box-shadow","0px 2px 5px 2px rgba(140, 140, 140, 0.4)");
+// 	} else {
+// 		$(".header").css("background","none");
+// 		$(".header").css("box-shadow","none");
+// 	}
+// 	if($(this).scrollTop() > 500) {
+// 		$("#menu li a").css("margin-left","50px");
+// 		$("#menu li a").css("font-size","1.3rem");
+// 		$("#menu li a").css("font-weight","bold");
+// 		$("#menu li a").css("color","#000");
+// 	} else {
+// 		$("#menu li a").css("margin-left","40px");
+// 		$("#menu li a").css("font-size","1.4rem");
+// 		$("#menu li a").css("font-weight","normal");
+// 		$("#menu li a").css("color","#fff");
+// 	}
+// 	if($(this).scrollTop() > 500) {
+// 		$(".header-logo a").css("width","150px");
+// 	} else {
+// 		$(".header-logo a").css("width","175px");
+// 	}
+// });
